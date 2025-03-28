10de77b691f7595564e4a68fdd2006a15fb07b61
Michael Beier <admin@mbeier.at>
2025-03-28 22:44:55 +0100

    added reflections to question type

--- src/main/java/at/mbeier/examassistant/model/questions/QuestionTypes.java
+++ src/main/java/at/mbeier/examassistant/model/questions/QuestionTypes.java
@@ -1,19 +1,21 @@
 package at.mbeier.examassistant.model.questions;
 
 public enum QuestionTypes {
-    MULTIPLE_CHOICE_QUESTION("MultipleChoice"),
-    SINGLE_CHOICE_QUESTION("SingleChoice"),
-    ALL_OR_NOTHING_QUESTION("AllOrNothing"),
-    TRUE_FALSE_QUESTION("True/False"),
-    SHORT_ANSWER_QUESTION("ShortAnswer"),
-    NUMERICAL_QUESTION("Numerical"),
-    MATCHING_QUESTION("Matching"),
-    ESSAY_QUESTION("Essay"),
-    CLOZE_QUESTION("Cloze"),;
+    MULTIPLE_CHOICE_QUESTION("MultipleChoice", MultipleChoiceQuestion.class),
+    SINGLE_CHOICE_QUESTION("SingleChoice", SingleChoiceQuestion.class),
+    ALL_OR_NOTHING_MC_QUESTION("AllOrNothing", AllOrNothingMCQuestion.class),
+    TRUE_FALSE_QUESTION("True/False", TrueFalseQuestion.class),
+    SHORT_ANSWER_QUESTION("ShortAnswer", ShortAnswerQuestion.class),
+    NUMERICAL_QUESTION("Numerical", NumericalQuestion.class),
+    MATCHING_QUESTION("Matching", MatchingQuestion.class),
+    ESSAY_QUESTION("Essay", EssayQuestion.class),
+    CLOZE_QUESTION("Cloze", ClozeQuestion.class),;
 
     private String identifier;
+    private Class<? extends Question> questionClass;
 
-    QuestionTypes(String identifier) {
+    QuestionTypes(String identifier, Class<? extends Question> questionClass) {
         this.identifier = identifier;
+        this.questionClass = questionClass;
     }
 }
