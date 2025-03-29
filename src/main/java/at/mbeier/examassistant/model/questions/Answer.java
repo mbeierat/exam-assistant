bf864664401e7c4a30b7ca5767138430bbd1f2d8
Michael Beier <admin@mbeier.at>
2025-03-29 02:38:43 +0100

    implemented answer class

--- src/main/java/at/mbeier/examassistant/model/questions/Answer.java
+++ src/main/java/at/mbeier/examassistant/model/questions/Answer.java
@@ -1,6 +1,11 @@
 package at.mbeier.examassistant.model.questions;
 
-public class Answer {
+import at.mbeier.examassistant.model.GIFTFormattable;
+import at.mbeier.examassistant.model.MoodleXMLFormattable;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class Answer implements GIFTFormattable, MoodleXMLFormattable {
 
     private final String answer;
     private final boolean correct;
@@ -10,5 +15,34 @@ public class Answer {
         this.answer = answer;
         this.correct = correct;
         this.weight = weight;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public Percentage getPercentage() {
+        return weight;
+    }
+
+    @Override
+    public String toGIFTString() {
+        return "~" + this.getPercentage().getGIFTValue() + "[html]" + this.answer;
+    }
+
+    @Override
+    public void appendXMLElements(Document doc, Element parent) {
+        Element answer = doc.createElement("answer");
+        answer.setAttribute("fraction", this.getPercentage().getXMLValue());
+        answer.setAttribute("format", "html");
+        parent.appendChild(answer);
+        Element text = doc.createElement("text");
+        text.setNodeValue(this.getAnswer());
+        answer.appendChild(text);
+        Element feedback = doc.createElement("feedback");
+        feedback.setAttribute("format", "html");
+        answer.appendChild(feedback);
+        Element feedbackText = doc.createElement("text");
+        feedback.appendChild(feedbackText);
     }
 }
