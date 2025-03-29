7d4eaae6a5682df447283b2c68140a5e1ced6456
Michael Beier <admin@mbeier.at>
2025-03-29 03:12:01 +0100

    implemented essay question

--- src/main/java/at/mbeier/examassistant/model/questions/EssayQuestion.java
+++ src/main/java/at/mbeier/examassistant/model/questions/EssayQuestion.java
@@ -1,8 +1,37 @@
 package at.mbeier.examassistant.model.questions;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 public class EssayQuestion extends Question {
 
     public EssayQuestion(String title, String question, String points, String[] answers) {
         super(title, question, points);
+        if (answers != null) {
+            if (answers.length > 0) throw new IllegalArgumentException("Answer values for Essay Questions are not supported.");
+        }
+    }
+
+    @Override
+    public String toGIFTString() {
+        return super.toGIFTString() + "{}";
+    }
+
+    @Override
+    public void appendXMLElements(Document doc, Element parent) {
+        Element question = doc.createElement("question");
+        question.setAttribute("type", "essay");
+        parent.appendChild(question);
+        super.appendXMLElements(doc, question);
+        Element answer = doc.createElement("answer");
+        answer.setAttribute("fraction", "0");
+        question.appendChild(answer);
+        Element answerText = doc.createElement("text");
+        answer.appendChild(answerText);
+    }
+
+    @Override
+    public String toString() {
+        return "EssayQuestion{" + super.toString() + '}';
     }
 }
