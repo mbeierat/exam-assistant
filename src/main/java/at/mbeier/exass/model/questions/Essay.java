e437b2914be4ceeeb749a32ac52cb8d9b329e193
Michael Beier <admin@mbeier.at>
2026-08-22 00:57:57 +0200

    implemented Essay Questions

--- src/main/java/at/mbeier/exass/model/questions/Essay.java
+++ src/main/java/at/mbeier/exass/model/questions/Essay.java
@@ -1,6 +1,7 @@
 package at.mbeier.exass.model.questions;
 
 import at.mbeier.exass.excel.ExcelRow;
+import at.mbeier.exass.excel.ImportUtil;
 import at.mbeier.exass.exporter.XMLUtil;
 import at.mbeier.exass.model.Question;
 import at.mbeier.exass.model.QuestionType;
@@ -13,21 +14,35 @@ public class Essay extends Question {
         super();
     }
 
-    public Essay(String name, String text, int points) {
-        super(QuestionType.ESSAY, name, text, points);
+    public Essay(String title, String text, int points) {
+        super(QuestionType.ESSAY, title, text, points);
     }
 
     @Override
     public void createFrom(ExcelRow row) {
+        ImportUtil.setStandardQuestionFields(this, row);
     }
 
     @Override
     public String toGIFTString() {
-        return "";
+        return "::" + super.getTitle() + "::" +
+                "[html]" + super.getText() + " {}";
     }
 
     @Override
     public Element toXMLElement(Document doc) {
-        return XMLUtil.element(doc, "question");
+        Element question = XMLUtil.question(doc, "essay");
+        XMLUtil.append(question,
+                XMLUtil.moodleText(doc, "name", super.getTitle()),
+                XMLUtil.moodleText(doc, "questiontext", "html", super.getText()),
+                XMLUtil.moodleText(doc, "defaultgrade", super.getPoints() + ""),
+                XMLUtil.moodleText(doc, "responseformat", "editor"),
+                XMLUtil.moodleText(doc, "responserequired", "1"),
+                XMLUtil.moodleText(doc, "responsefieldlines", "20"),
+                XMLUtil.moodleText(doc, "minwordlimit", ""),
+                XMLUtil.moodleText(doc, "maxwordlimit", ""),
+                XMLUtil.moodleText(doc, "graderinfo", "html", ""),
+                XMLUtil.moodleText(doc, "responsetemplate", "html", ""));
+        return question;
     }
 }
